a99cbd877b23cc0db2045439ccaa5287d8554639
Patdanai Tirathanapat <6733192121@student.chula.ac.th>
2025-04-20 19:00:30 +0700

    Profile renter card

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,12 @@
 import OrderList from "./OrderList";
+import ProfileCard from "./ProfileCard";
 
 export default function Dashboard(){
     return (
             <div className="w-[100%] h-[90%] grid grid-rows-[auto_1fr_auto] grid-cols-2 gap-4 bg-white p-5">
 
                 <div className="col-span-2 flex justify-center">
-                    <div className="w-[300px] h-[25vh] bg-pink-300 rounded-md shadow-md "></div>
+                    <ProfileCard />
                 </div>
             <div className="col-span-2 flex justify-center space-x-5">
                     <div className="h-[40vh] w-[50vw] bg-yellow-300 rounded-md shadow-md"></div>
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useSession } from 'next-auth/react';
+
+type ProfileResponse = {
+  success: boolean;
+  data: {
+    name: string;
+  };
+};
+
+export default function ProfileCard() {
+  const [name, setName] = useState("");
+  const { data: session } = useSession();
+
+  return (
+    <div className="w-[500px] h-[10vh]">
+      <h1 style={{
+        fontSize: "2rem",
+        fontWeight: "bold",
+        textAlign:"center",
+        color: "#000",
+      }}>
+        {session?.user?.name || "Loading..."}
+      </h1>
+    </div>
+  );
+}
