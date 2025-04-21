545a36b60c96ad3cc3bfbfa45326e153988ab54d
gincole <gc.gincole@gmail.com>
2025-04-21 23:18:41 +0700

    add: renter page

---
+++ src/app/(renter)/renter/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DevelopingPage() {
+    return (
+      <main className="flex items-center justify-center h-screen bg-white">
+        <h1 className="text-3xl font-semibold text-gray-500">Developing...</h1>
+      </main>
+    );
+  }
+  
