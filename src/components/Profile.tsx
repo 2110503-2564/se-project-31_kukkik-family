e1b8118dd1f19fee47e055a2b32a07071f0db064
tkkxttys <kittycatty.2548@gmail.com>
2025-04-23 00:07:49 +0700

    add User, UserJson interface

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -33,9 +33,30 @@ export default async function CarCatalog({CarProviderJson}: {CarProviderJson: Pr
 } */
 
 
-import { useRouter } from "next/navigation";
+/* import { useRouter } from "next/navigation";
 
 export default function Profile() {
+    const router = useRouter();
+    return (
+        <main className="min-h-screen bg-[#FFE5B4] flex flex-col items-center justify-center p-6 space-y-6">
+            <div className="text-2xl font-semibold">{ *//*ใส่ชื่อ user; get name */{/* }</div>
+   
+            <div className="bg-white rounded-2xl shadow-lg p-6 w-[90%] max-w-md text-lg space-y-2 text-center">
+                <div>tel. : </div>
+                <div>email. : </div>
+            </div>
+
+            <div className="bg-grey rounded-2xl shadow-lg p-6 w-[90%] max-w-md text-lg space-y-2 text-center"> */}
+                {/*ใส่ 3 product card รถ 3 คัน*/}
+{/*             </div>
+        </main>
+    );
+} */}
+
+import { useRouter } from "next/navigation";
+
+export default async function Profile({UserJson}: {UserJson: Promise<UserJson>;}) {
+    const UserJsonReady = await UserJson;
     const router = useRouter();
     return (
         <main className="min-h-screen bg-[#FFE5B4] flex flex-col items-center justify-center p-6 space-y-6">
