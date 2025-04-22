b7233810ac92eb008b32cfb480b914dc0659e32e
tkkxttys <kittycatty.2548@gmail.com>
2025-04-22 23:43:33 +0700

    วางโครง

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,29 +1,20 @@
-/* import OrderList from "./OrderList";
-import ProfileCard from "./ProfileCard";
 import { useRouter } from "next/navigation";
-import AllTimeIncome from "./AllTimeIncome"
 
-export default function Dashboard(){
+export default function Profile() {
     const router = useRouter();
     return (
-            <div className="w-[100%] h-[90%] bg-[#FFE5B4] grid grid-rows-[auto_1fr_auto] grid-cols-2 gap-4 p-5 space-y-10">
-
-                <div className="col-span-2 flex justify-center">
-                    <ProfileCard />
-                </div>
-                <div className="col-span-2 flex justify-center space-x-5">
-                <AllTimeIncome />
-                <OrderList />
-                </div>
-                <div className="col-span-2 flex justify-center ">
-                    <div className="w-[300px] h-[10vh] bg-lime-300 rounded-md shadow-md flex items-center justify-center cursor-pointer hover:bg-lime-400 transition-colors duration-200"
-                    onClick={() => router.push('/wallet')}>
-                        <h1 className="text-xl font-bold text-gray-800 tracking-wide">
-                            Go To Wallet
-                        </h1>
-                    </div>
-                </div>
-
+        <main className="min-h-screen bg-[#FFE5B4] flex flex-col items-center justify-center p-6 space-y-6">
+            <div className="text-2xl font-semibold">{/*ใส่ชื่อ user; get name */}</div>
+   
+            <div className="bg-white rounded-2xl shadow-lg p-6 w-[90%] max-w-md text-lg space-y-2 text-center">
+                <div>tel. : </div>
+                <div>email. : </div>
             </div>
-    )
-}    */
+
+            <div className="bg-grey rounded-2xl shadow-lg p-6 w-[90%] max-w-md text-lg space-y-2 text-center">
+                {/*ใส่ 3 product card รถ 3 คัน*/}
+            </div>
+        </main>
+    );
+}
+    
