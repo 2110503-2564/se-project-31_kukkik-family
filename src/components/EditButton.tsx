d3abb21285ec296966ba8d30be6431aa851e8109
puckanut <puckanut2549@gmail.com>
2025-04-25 22:15:36 +0700

    add pop up

--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -1,15 +1,45 @@
-import { Button } from "@mui/material";
-import Image from "next/image";
+import { useState } from 'react';
+import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from '@mui/material';
+import Image from 'next/image';
 
-export default function EditButton(){
-    return (
-        <Button>
-            <Image
-                src={"/img/edit.png"}
-                alt="logo"
-                width={25}
-                height={25}
-            />
-        </Button>
-    )
+export default function EditButton() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>
+        <Image src="/img/edit.png" alt="edit" width={25} height={25} />
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit User</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            defaultValue="John Doe"
+          />
+          <TextField
+            margin="dense"
+            label="Email"
+            type="email"
+            fullWidth
+            variant="standard"
+            defaultValue="john@example.com"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} variant="contained">Save</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
