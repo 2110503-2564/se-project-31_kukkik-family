e04fa541139cd900b713cd513d104aac259212bc
gincole <gc.gincole@gmail.com>
2025-04-26 22:32:32 +0700

    fix: fetch api

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -31,8 +31,8 @@ export default function RegisterPage() {
       }
       console.log( dataToSend );
 
-      // const res = await fetch("https://api-coin-kukkik.vercel.app/api/v1/auth/register", {
-      const res = await fetch('http://localhost:5000/api/v1/auth/register', {
+      const res = await fetch("https://api-coin-kukkik.vercel.app/api/v1/auth/register", {
+      // const res = await fetch('http://localhost:5000/api/v1/auth/register', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
