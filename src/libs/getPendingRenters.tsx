ca48a32eb0b189246f52c36160f51d6fdf61e8a3
Sinsuda <sinsuda.rakporpiang@gmail.com>
2025-04-26 21:55:38 +0700

    revert merge

--- src/libs/getPendingRenters.tsx
+++ src/libs/getPendingRenters.tsx
@@ -1,7 +1,7 @@
 // libs/getPendingRenters.ts
 export default async function getPendingRenters(token:string) {
     // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/users/renter-requests`, {
-        const res = await fetch(`http://localhost:5000/api/v1/users/renter-requests`, {
+  const res = await fetch(`https://se-project-backend-31-kukkik-family.vercel.app/api/v1/users/renter-requests`, {
       cache: 'no-store',
       headers: {
         'Authorization': `Bearer ${token}`, // ถ้ามีการยืนยันตัวตน
