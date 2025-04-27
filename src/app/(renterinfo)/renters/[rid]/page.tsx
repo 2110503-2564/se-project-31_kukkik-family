93a5e7c512879c6584f46b114c4199e42d32d370
Sinsuda <sinsuda.rakporpiang@gmail.com>
2025-04-27 13:59:14 +0700

    เราไปเปลี่ยนlayout card มาแล้วทำให้การ์ดคลิกได้ ละฟิกสีข้อความโปรไฟล์ให้กำ รันในเครื่องเรามันขาว

--- src/app/(renterinfo)/renters/[rid]/page.tsx
+++ src/app/(renterinfo)/renters/[rid]/page.tsx
@@ -5,6 +5,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { redirect } from "next/navigation";
 import CarRenterCard from "@/components/CarRenterCard";
 import EditButton from "@/components/EditButton";
+import Link from "next/link";
 
 export default async function RenterProfilePage({ params }: { params: { rid: string } }) {
 
@@ -21,13 +22,13 @@ export default async function RenterProfilePage({ params }: { params: { rid: str
 
   return (
     <div className="p-6 bg-[#FFE5B4] flex flex-col justify-center items-center gap-6">
-      <div className="text-5xl font-bold">{bookingRes.name}</div>
+      <div className="text-5xl font-bold text-black">{bookingRes.name}</div>
       <div className="bg-white text-2xl py-8 px-20 rounded-lg shadow-md flex flex-col gap-4 w-fit relative">
-        <div>
-          <span className="font-bold">tel. :</span> {bookingRes.tel}
+        <div className="text-black">
+          <span className="font-bold text-black">tel. :</span> {bookingRes.tel}
         </div>
-        <div>
-          <span className="font-bold">email :</span> {bookingRes.email}
+        <div className=" text-black">
+          <span className="font-bold text-black">email :</span> {bookingRes.email}
         </div>
         <div className="absolute bottom-4 right-4">
           <EditButton/>
@@ -35,21 +36,25 @@ export default async function RenterProfilePage({ params }: { params: { rid: str
         
       </div>
 
+      <div className="m-4 flex flex-row content-around justify-around flex-wrap gap-3">
+        {bookedCars.map((car) => (
+          <Link href={`/cars/${car.id}`} className="w-[250px] flex" prefetch={true}>
+            <CarRenterCard
+              key={car.id}
+              carId={car.id}
+              carName={car.name}
+              imgSrc={car.picture}
+              price={car.dailyrate}
+              seat={car.seat}
+              like={car.like}
+              province={car.province}
+            />
+          </Link>
+        ))}
+
+      </div>
 
 
-
-      {bookedCars.map((car) => (
-        <CarRenterCard
-          key={car.id}
-          carId={car.id}
-          carName={car.name}
-          imgSrc={car.picture}
-          price={car.dailyrate}
-          seat={car.seat}
-          like={car.like}
-          province={car.province}
-        />
-      ))}
     </div>
   );
 }
