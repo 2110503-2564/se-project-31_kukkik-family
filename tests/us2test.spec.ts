77f1e1feb1c8d0d3e5dfdaadf382204d95f9f50f
tkkxttys <kittycatty.2548@gmail.com>
2025-04-27 15:54:38 +0700

    edit test 2 function

--- tests/us2test.spec.ts
+++ tests/us2test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('login', async ({ page }) => {
+test.beforeEach('login', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await expect(page).toHaveTitle(/Create Next App/);
